9358eb56ed773bed10c081da25922b94f27027b1
Rodion Kostin <itrodion@gmail.com>
2021-03-14 22:15:03 +0300

    03_ThreeWayAndTkinter: Change size from rectangle to square

--- 03_ThreeWayAndTkinter/main.py
+++ 03_ThreeWayAndTkinter/main.py
@@ -19,14 +19,14 @@ class Application(tk.Frame):
     """
     Init app settings.
     """
-    self.playgroundSize = (4, 4)
+    self.playgroundSize = 4
 
   def initPlayState(self):
     """
     Init play state
     """
-    width, height = self.playgroundSize
-    self.emptyCell = (width - 1, height - 1)
+    size = self.playgroundSize
+    self.emptyCell = (size - 1, size - 1)
 
   def initUI(self):
     """
@@ -42,7 +42,7 @@ class Application(tk.Frame):
     self.btn_New = tk.Button(self, text='New', command=self.startNewGame)
     self.btn_Exit = tk.Button(self, text='Exit', command=self.quit)
 
-    columnSpan = self.playgroundSize[1] // 2
+    columnSpan = self.playgroundSize // 2
     self.btn_New.grid(row=0, column=0, columnspan=columnSpan)
     self.btn_Exit.grid(row=0, column=columnSpan, columnspan=columnSpan)
 
@@ -50,7 +50,7 @@ class Application(tk.Frame):
     """
     Init playground.
     """
-    playgroundAreaSize = self.playgroundSize[0] * self.playgroundSize[1]
+    playgroundAreaSize = self.playgroundSize ** 2
     for i in range(1, playgroundAreaSize):
       btnName = f'btn_Play{i}'
       def playHandler(number=i): 
@@ -66,19 +66,19 @@ class Application(tk.Frame):
     """
     print('New game')
 
-    self.emptyCell = (self.playgroundSize[0] - 1, self.playgroundSize[1] - 1)
-    playgroundIter = self.makeNewPlaygroundSequence(*self.playgroundSize)
+    self.emptyCell = (self.playgroundSize - 1, self.playgroundSize - 1)
+    playgroundIter = self.makeNewPlaygroundSequence(self.playgroundSize)
     j = -1
     missCount = 0
     for i in playgroundIter:
       j += 1
-      rowIndex = 1 + j // self.playgroundSize[0]
-      columnIndex = j % self.playgroundSize[1]
+      rowIndex = 1 + j // self.playgroundSize
+      columnIndex = j % self.playgroundSize
       btn = getattr(self, f'btn_Play{i}')
       btn.grid(row=rowIndex, column=columnIndex, sticky=tk.N+tk.E+tk.S+tk.W)
       
-      okRowIndex = 1 + i // self.playgroundSize[0]
-      okColumnIndex = i % self.playgroundSize[1]
+      okRowIndex = 1 + i // self.playgroundSize
+      okColumnIndex = i % self.playgroundSize
       missCount += int(rowIndex != okRowIndex or columnIndex != okColumnIndex)
 
     if missCount == 0:
@@ -103,9 +103,9 @@ class Application(tk.Frame):
   def tryToWin(self):
     win = True
 
-    for i in range(self.playgroundSize[0] * self.playgroundSize[1] - 1):
-        irow = 1 + i // self.playgroundSize[0]
-        icolumn = i % self.playgroundSize[1]
+    for i in range(self.playgroundSize ** 2 - 1):
+        irow = 1 + i // self.playgroundSize
+        icolumn = i % self.playgroundSize
 
         btnGridInfo = getattr(self, f'btn_Play{1 + i}').grid_info()
         row, column = btnGridInfo['row'], btnGridInfo['column']
@@ -123,11 +123,11 @@ class Application(tk.Frame):
     """
     return abs(self.emptyCell[0] + 1 - row) + abs(self.emptyCell[1] - column) <= 1
 
-  def makeNewPlaygroundSequence(self, width, height):
+  def makeNewPlaygroundSequence(self, size):
     """
     Make new play sequence for `width*height` playground.
     """
-    playgroundAreaSize = width * height
+    playgroundAreaSize = size ** 2
     playgroundIter = iter(sample(range(1, playgroundAreaSize), playgroundAreaSize - 1))
 
     return playgroundIter
