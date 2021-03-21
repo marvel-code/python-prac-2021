a92c80ae36684fa9319ff5395d3536cec51b28a7
Rodion Kostin <itrodion@gmail.com>
2021-03-22 01:21:10 +0300

    05: Init UI

---
+++ 05_SshAndSmartWidgents/main.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import inspect
+
+class Application(tk.Frame):
+    '''Sample tkinter application class'''
+
+    def __init__(self, master=None, title="<application>", **kwargs):
+        '''Create root window with frame, tune weight and resize'''
+        super().__init__(master, **kwargs)
+        self.master.title(title)
+        self.master.columnconfigure(0, weight=1)
+        self.master.rowconfigure(0, weight=1)
+        self.grid(sticky="NEWS")
+        self.create_widgets()
+        for column in range(self.grid_size()[0]):
+            self.columnconfigure(column, weight=1)
+        for row in range(self.grid_size()[1]):
+            self.rowconfigure(row, weight=1)
+
+    def create_widgets(self):
+        '''Create all the widgets'''
+
+class App(Application):
+    def create_widgets(self):
+      t = tk.Text(self)
+      c = tk.Canvas(self)
+      c.grid()
+      t.grid()
+      
+
+app = App(title="Sample application")
+app.mainloop()
